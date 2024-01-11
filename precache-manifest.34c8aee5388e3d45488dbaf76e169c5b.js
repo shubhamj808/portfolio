ac705e76e0bcacf6ecc6991d7d8b3aae29d7dcd4
Shubham Jadhav <shubhamjadhav@IN-FN4R031GQ0.local>
2024-01-11 18:55:05 +0530

    Updates

---
+++ precache-manifest.34c8aee5388e3d45488dbaf76e169c5b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60a49ac408b8149c8b29b53a3940bb96",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "bef816422e712020b7b7",
+    "url": "/portfolio/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "9bb1b39de2f65b64b0c5",
+    "url": "/portfolio/static/js/2.7025d2da.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/portfolio/static/js/2.7025d2da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3be833cb67787fe2f8ab",
+    "url": "/portfolio/static/js/3.56582566.chunk.js"
+  },
+  {
+    "revision": "bef816422e712020b7b7",
+    "url": "/portfolio/static/js/main.0ce6f2c6.chunk.js"
+  },
+  {
+    "revision": "4d12e027882441e847b8",
+    "url": "/portfolio/static/js/runtime-main.482ede98.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/portfolio/static/media/logo.06e73328.svg"
+  }
+]);
