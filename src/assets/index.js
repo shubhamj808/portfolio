e1a20a57eb0e6a9bcae78606d3ce49b5eef29eec
Shubham Jadhav <shubhamjadhav@IN-FN4R031GQ0.local>
2024-01-15 17:49:28 +0530

    components added

---
+++ src/assets/index.js
@@ -0,0 +1,66 @@
+import logo from "./sj.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import heroImage from "./hero.png";
+
+import docker from "./tech/docker.png";
+import git from "./tech/git.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import kubernetes from "./tech/kubernetes.png";
+import java from "./tech/java.png";
+import dotnet from "./tech/dotnet.png";
+import azure from "./tech/azure.png";
+import prometheus from "./tech/prometheus.svg";
+import elastic from "./tech/elasticsearch.svg";
+import terraform from "./tech/terraform.png";
+
+import meta from "./company/meta.png";
+import shopify from "./company/shopify.png";
+import starbucks from "./company/starbucks.png";
+import tesla from "./company/tesla.png";
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+
+  heroImage,
+
+  docker,
+  git,
+  mongodb,
+  nodejs,
+  reactjs,
+  kubernetes,
+  java,
+  dotnet,
+  azure,
+  prometheus,
+  elastic,
+  terraform,
+  
+  meta,
+  shopify,
+  starbucks,
+  tesla,
+  carrent,
+  jobit,
+  tripguide,
+};
